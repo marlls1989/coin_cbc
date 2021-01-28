ea8d2fcad6b1bdfba4451efd602972d81b325209
Marcos Sartori <marcos.sartori@acad.pucrs.br>
2021-01-28 12:18:02 -0200

    using features flags to enable embbeding of cbc

--- coin_cbc_sys/build.rs
+++ coin_cbc_sys/build.rs
@@ -3,22 +3,24 @@ use std::{env, path::Path, process::Command};
 use system_deps;
 
 fn main() {
+    #[cfg(feature = "embedded_cbc")]
+    {
+        env::set_var("SYSTEM_DEPS_CBC_BUILD_INTERNAL", "always");
+    }
+
     system_deps::Config::new()
         .add_build_internal("cbc", |lib, version| {
             let cores = num_cpus::get();
             let out_dir = env::var("OUT_DIR").unwrap();
             let src_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-            let deps_dir = format!("{}/deps", src_dir);
-            let dest_dir = format!("{}/cbc", out_dir);
-
-            Command::new(&format!("{}/coinbrew", deps_dir))
-                .args(&["fetch", "Cbc@2.10.5"])
-                .current_dir(&Path::new(&deps_dir))
+            Command::new(&format!("{}/coinbrew", src_dir))
+                .args(&["fetch", "Cbc@2.10.5", "--no-prompt"])
+                .current_dir(&Path::new(&out_dir))
                 .status()
                 .unwrap();
 
-            Command::new(&format!("{}/coinbrew", deps_dir))
+            Command::new(&format!("{}/coinbrew", src_dir))
                 .args(&[
                     "build",
                     "Cbc",
@@ -26,17 +28,18 @@ fn main() {
                     "--no-prompt",
                     "--tests=none",
                     &format!("--parallel-jobs={}", cores),
-                    &format!("--prefix={}", dest_dir),
                     "ADD_FFLAGS=-fallow-argument-mismatch -fPIC",
                     "ADD_CFLAGS=-fPIC",
                     "ADD_CXXFLAGS=-fPIC",
                 ])
-                .current_dir(&Path::new(&deps_dir))
+                .current_dir(&Path::new(&out_dir))
                 .status()
                 .unwrap();
 
+            println!("cargo:rustc-link-lib=static=stdc++");
+
             system_deps::Library::from_internal_pkg_config(
-                format!("{}/lib/pkgconfig", dest_dir),
+                format!("{}/dist/lib/pkgconfig", out_dir),
                 lib,
                 version,
             )
